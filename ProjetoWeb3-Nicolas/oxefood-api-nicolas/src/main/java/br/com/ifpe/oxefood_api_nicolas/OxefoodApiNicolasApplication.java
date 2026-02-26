7f3a8adba88238529f6566a42c96c46da8b8b235
Nicolas-Silva01 <nsbf@discente.ifpe.edu.br>
2026-02-26 15:33:43 -0300

    Add files via upload

---
+++ ProjetoWeb3-Nicolas/oxefood-api-nicolas/src/main/java/br/com/ifpe/oxefood_api_nicolas/OxefoodApiNicolasApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood_api_nicolas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiNicolasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiNicolasApplication.class, args);
+	}
+
+}
---
+++ ProjetoWeb3-Nicolas/oxefood-api-nicolas/src/test/java/br/com/ifpe/oxefood_api_nicolas/OxefoodApiNicolasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood_api_nicolas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodApiNicolasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
