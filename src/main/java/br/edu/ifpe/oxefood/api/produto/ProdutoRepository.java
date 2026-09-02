073538798fc8de55f5800a2188e33cb8901335cb
gabrielysales <gvvs@discente.ifpe.edu.br>
2026-09-01 21:23:55 -0300

    quarta aula

---
+++ src/main/java/br/edu/ifpe/oxefood/api/produto/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package br.edu.ifpe.oxefood.api.produto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    
+}
+
